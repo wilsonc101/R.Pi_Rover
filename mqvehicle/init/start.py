f501d9eaad1960fe134e3f468fded9d7ccb075d7
Chris <wilsonc101@gmail.com>
2015-04-23 22:10:56 +0100

    temp fix for orphan processes

--- mqvehicle/init/start.py
+++ mqvehicle/init/start.py
@@ -4,12 +4,12 @@ PIDFILE_1="/var/run/pi_rover.pid"
 case "$1" in
   start)
     echo "Preparing GPS...."
-    /usr/bin/killall gpsd
+    /usr/bin/killall -9 gpsd
     /usr/sbin/gpsd /dev/ttyAMA0 -F /var/run/gpsd.sock -n
     echo "....done"
 
     echo "Starting Pi Rover...."
-    /usr/bin/killall pi_rover
+    /usr/bin/killall -9 pi_rover
     /sbin/start-stop-daemon --start --pidfile $PIDFILE_1 -b --make-pidfile --exec $DAEMON_1
     echo "....done"
     ;;
@@ -17,22 +17,29 @@ case "$1" in
   stop)
     echo "Stopping Pi Rover...."
     /sbin/start-stop-daemon --stop --pidfile $PIDFILE_1
+    /usr/bin/killall -9 gpsd
+    /usr/bin/killall -9 pi_rover
     echo "....done"
     ;;
 
   restart)
     echo "Stopping Pi Rover...."
     /sbin/start-stop-daemon --stop --pidfile $PIDFILE_1
+    /usr/bin/killall -9 gpsd
+    /usr/bin/killall -9 pi_rover
     echo "....done"
 
     sleep 2s
     
     echo
+    echo "Preparing GPS...."
+    /usr/sbin/gpsd /dev/ttyAMA0 -F /var/run/gpsd.sock -n
+    echo "....done"
+
     echo "Starting Pi Rover...."
     /sbin/start-stop-daemon --start --pidfile $PIDFILE_1 -b --make-pidfile --exec $DAEMON_1
     echo "....done"
     ;;
-
 
 
 
