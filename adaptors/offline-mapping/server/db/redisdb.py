3cdfb1d5cfa1ac50dd905063eded5bf65b72a8a6
Chris Wilson <wilsonc101@gmail.com>
2015-12-31 00:57:07 +0000

    data returned to js

--- adaptors/offline-mapping/server/db/redisdb.py
+++ adaptors/offline-mapping/server/db/redisdb.py
@@ -1,4 +1,7 @@
 import redis
+import json
+
+import ast
 
 REDIS_SERVER = "localhost"
 REDIS_SERVER_PORT = 6379
@@ -9,13 +12,69 @@ REDIS_CONNECTION = redis.StrictRedis(host=REDIS_SERVER,
                                      db=REDIS_DB)
 
 def deleteObject(id):
-    nothing = None
-    
-def createObject(id):
-    nothing = None
-    return "Things"
-    
-def updateObject(id):
-    nothing = None
+    REDIS_CONNECTION.delete(id)
+
+    return True
 
     
+def createObject(id, type, data):
+    json_data = json.loads(data)
+
+    object_data = {}
+    object_data['type'] = str(type)
+    object_data['coordinates'] = json_data['geometry']['coordinates']
+    
+    REDIS_CONNECTION.set(id,object_data)
+    
+    return True
+    
+    
+def updateObject(id, type, data):
+    json_data = json.loads(data)
+
+    # Get type property from original object
+    original_object = REDIS_CONNECTION.get(id)
+    type = dict(ast.literal_eval(original_object))['type']
+
+    object_data = {}
+    object_data['type'] = type
+    object_data['coordinates'] = json_data['geometry']['coordinates']
+    
+    REDIS_CONNECTION.set(id,object_data)
+    
+    return True
+
+
+def getObjects():
+    geoobjects = {}
+    object_ids = REDIS_CONNECTION.keys()
+    
+    for id in object_ids:
+        geoobject = dict(ast.literal_eval(REDIS_CONNECTION.get(id)))
+        geoobjects[id] = geoobject
+        
+    return json.dumps(geoobjects)
+
+
+def _correctcoords(data):
+    if data['type'] == "marker":
+        coordinates = str(data['data']['geometry']['coordinates'])
+    
+    elif data['type'] == "polyline":
+        coordinates =  str(data['data']['geometry']['coordinates'])
+
+    elif data['type'] == "polygon":
+        coordinates =  str(data['data']['geometry']['coordinates'][0])
+    
+    elif data['type'] == "rectangle":
+        coordinates =  str(data['data']['geometry']['coordinates'][0])
+
+    elif data['type'] == "circle": 
+        coordinates =  str(data['data']['geometry']['coordinates'])
+    
+    return 
+    
+#{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[-2.14583158493042,51.44720046655099]}}
+#{"type":"Feature","properties":{},"geometry":{"type":"LineString","coordinates":[[-2.1425271034240723,51.448083088192334],
+#                                                                                 [-2.1448874473571777,51.44694637534],
+#                                                                                 [-2.1454238891601562,51.44564915080628]]}}
--- adaptors/offline-mapping/server/index.py
+++ adaptors/offline-mapping/server/index.py
@@ -8,20 +8,35 @@ db = apache.import_module('db/redisdb')
 
 def index(req):
     method = req.method
-    with open("/tmp/test.log", "a") as temp_log:
+    
+    # Method specific actions
+    if method == "POST":
+        # Extract HTTP form data
+        data = req.form
 
-        if method == "POST":
-            data = req.form
-            temp_log.write(" ---------- " + str(req.method) + " ---------- \n" +
-                           data['action'] + "\n" +
-                           data['id'] + "\n" +
-                           data['type'] + "\n" +
-                           data['data'] + "\n" +
-                           "-----------------------------\n") 
-            return "POST OK"
+        action = data['action']
+        object_id = data['id']
+        object_type = data['type']
+        geo_data = data['data']
+
+        # Map object actions  
+        if action == "create":
+            db.createObject(id=object_id, 
+                            type=object_type,
+                            data=geo_data)
+            
+        elif action == "delete":
+            db.deleteObject(id=object_id)
+            
+            
+        elif action == "edit":
+            db.updateObject(id=object_id, 
+                            type=object_type,
+                            data=geo_data)
+            
+        return "POST OK"
 
 
-        elif method == "GET":
-            temp_log.write(" ---------- " + str(req.method) + " ---------- \n" +
-                           "-----------------------------\n") 
-            return "GET OK"
+    elif method == "GET":
+        response_data = db.getObjects()
+        return response_data
